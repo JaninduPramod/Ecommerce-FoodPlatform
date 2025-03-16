2b94f830e030c942649b4cb5e9c2c0b014496306
Janindu Pramod <janindupramod@gmail.com>
2025-03-16 19:38:29 +0530

    1

---
+++ Front-End/app/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const NavBar =()=>
+{
+    return(
+        <>
+            <AppBar sx={{
+                color:'black',
+                height:'50px',
+                margin:'20px',
+                
+            }}>
+                Hello
+            </AppBar>
+        </>
+    );
+};
+export default NavBar;
