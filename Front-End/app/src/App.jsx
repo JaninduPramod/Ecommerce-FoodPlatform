86afd2b24b8f0431b652dd97282c811d84768948
Janindu Pramod <janindupramod@gmail.com>
2025-03-17 06:01:40 +0530

    navbar designed

--- Front-End/app/src/App.jsx
+++ Front-End/app/src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
-import NavBar from './components/navbar'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+import HomePage from "./pages/home";
+import ProfilePage from "./pages/profile";
+import LoginPage from "./pages/login";
 
 function App() {
-  
-
   return (
     <>
-      <NavBar/>
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Front-End/app/src/components/navbar.jsx
+++ Front-End/app/src/components/navbar.jsx
@@ -1,19 +1,173 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Paper, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import Person2Icon from "@mui/icons-material/Person2";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const NavBar =()=>
-{
-    return(
-        <>
-            <AppBar sx={{
-                color:'black',
-                height:'50px',
-                margin:'20px',
-                
-            }}>
-                Hello
-            </AppBar>
-        </>
-    );
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <AppBar sx={{ alignItems: "center" }}>
+        <Paper
+          elevation={3}
+          sx={{
+            height: "90px",
+            width: "90%",
+            backgroundColor: "white",
+            marginTop: 1,
+            borderRadius: "18px",
+            position: "fixed",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Box
+            sx={{
+              color: "black",
+              height: "75%",
+              width: "20%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Icon
+          </Box>
+          <Box
+            sx={{
+              height: "75%",
+              width: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+
+              gap: 5,
+            }}
+          >
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/home"
+              sx={{
+                fontFamily: "cursive",
+                color: "black",
+                textDecoration: "none",
+                ":hover": { color: "#ff7d01" },
+              }}
+            >
+              Home
+            </Typography>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/profile"
+              sx={{
+                fontFamily: "cursive",
+                color: "black",
+                textDecoration: "none",
+                ":hover": { color: "#ff7d01" },
+              }}
+            >
+              Profile
+            </Typography>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/profile"
+              sx={{
+                fontFamily: "cursive",
+                color: "black",
+                textDecoration: "none",
+                ":hover": { color: "#ff7d01" },
+              }}
+            >
+              Tab3
+            </Typography>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/profile"
+              sx={{
+                fontFamily: "cursive",
+                color: "black",
+                textDecoration: "none",
+                ":hover": { color: "#ff7d01" },
+              }}
+            >
+              Tab 4
+            </Typography>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/profile"
+              sx={{
+                fontFamily: "cursive",
+                color: "black",
+                textDecoration: "none",
+                ":hover": { color: "#ff7d01" },
+              }}
+            >
+              Tab 5
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              color: "black",
+              height: "75%",
+              width: "20%",
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+              justifyContent: "center",
+            }}
+          >
+            <SearchIcon
+              sx={{
+                fontSize: "30px",
+                ":hover": { color: "#ff7d01" },
+                cursor: "pointer",
+              }}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                ":hover": { color: "#ff7d01" },
+                cursor: "pointer",
+              }}
+              onClick={() => navigate("/login")}
+            >
+              <Person2Icon sx={{ fontSize: "30px" }} />
+              <Typography
+                component={Link}
+                sx={{
+                  fontSize: "16px",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+              >
+                Log in
+              </Typography>
+            </Box>
+            <Button
+              variant="contained"
+              startIcon={<ShoppingCartIcon />}
+              sx={{
+                backgroundColor: "#ff7d01",
+                height: "30px",
+                ":hover": { backgroundColor: "black" },
+              }}
+            >
+              1 Item
+            </Button>
+          </Box>
+        </Paper>
+      </AppBar>
+    </>
+  );
 };
 export default NavBar;
