0cc8d9996e04c7c3d500e470256dd91814f34757
Janindu Pramod <janindupramod@gmail.com>
2025-03-19 00:21:49 +0530

    connected

--- Front-End/app/src/App.jsx
+++ Front-End/app/src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react";
 import "./App.css";
 import React from "react";
-import Card from "./components/Card.jsx";
 import Footer from "./components/Footer.jsx";
 import HomePage from "./pages/Home.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
-import img from "./assets/img1.jpg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Onboarding from "./pages/onboarding";
 
 
 function App() {
   
 
     return (
-        <div className="app">
-            <HomePage /> 
-             <ProductPage />  
-            <Footer /> 
-        </div>
+        // <div className="app">
+        //     <HomePage /> 
+        //      
+        //     
+        // </div>
+        <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+          <Route path="/product" element={<ProductPage />} />
+        </Routes>
+        <Footer /> 
+      </Router>
         
     );
 }
---
+++ Front-End/app/src/pages/onboarding.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, Typography, Button, Container } from "@mui/material";
+import ImageListComponent from "../components/imglist";
+
+const Onboarding = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100vw",
+          height: "500px",
+          mt: "75px",
+          backgroundImage:
+            "url(https://freshgo-webibazaar.myshopify.com/cdn/shop/files/slider-1_8d15eebe-d99f-4800-af09-a24e70339c55.jpg?v=1625900666)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            marginLeft: "120px",
+            gap: "30px",
+          }}
+        >
+          <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+            Organic Fresh Fruits <br /> For Your Health
+          </Typography>
+          <Typography variant="label">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut <br /> labore et dolore magna aliqua.
+          </Typography>
+          <Button
+            sx={{
+              width: "140px",
+              height: "45px",
+              backgroundColor: "#ff7d01",
+              color: "white",
+              borderRadius: "12px",
+              ":hover": { backgroundColor: "black" },
+            }}
+          >
+            Shop Now
+          </Button>
+        </Box>
+      </Box>
+      <ImageListComponent />
+      <Container sx={{ mt: 10, display: "flex", justifyContent: "center" }}>
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Trending Products
+        </Typography>
+      </Container>
+      <Box sx={{ height: "800px" }}></Box>
+    </>
+  );
+};
+export default Onboarding;
