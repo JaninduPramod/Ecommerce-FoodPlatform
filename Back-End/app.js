2e0698d7df4ac4d1f519e33f17b582913755b0fd
Janindu Pramod <janindupramod@gmail.com>
2025-03-20 02:05:17 +0530

    created the first api's and tested

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const controller = require("./controller");
 
 app.use(cors()); //created a middle ware
 
@@ -11,6 +12,21 @@ app.use(
 ); //express.urlencoded is used to encode and use the MIME type data that coming from HTML
 
 app.use(express.json()); // all the request & responses are done with JSOn format
+
+// api's
+
+app.get("/users", (req, res) => {
+  controller.GetUsers((users) => {
+    res.send(users);
+  });
+});
+
+app.get("/userid", (req, res) => {
+  const id = req.query.id;
+  controller.userById((user) => {
+    res.send(user);
+  }, id);
+});
 
 module.exports = app;
 
