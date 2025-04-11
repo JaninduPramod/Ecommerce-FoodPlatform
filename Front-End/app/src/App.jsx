619a256b5871b7c0280d14366229d34340096eb2
nadun097 <nadunanjana31@gmail.com>
2025-04-12 03:18:11 +0530

    product management 002

--- Front-End/app/src/App.jsx
+++ Front-End/app/src/App.jsx
@@ -16,6 +16,7 @@ import Profile from "./pages/Profile.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import SupplierProfile from "./pages/SupplierProfile.jsx";
 import Admin from "./pages/Admin.jsx";
+import ProductManagement from "./pages/ProductManagement.jsx";
 
 const AppLayout = () => {
   const location = useLocation();
@@ -35,6 +36,7 @@ const AppLayout = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/supplierPage" element={<SupplierProfile />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/productManagement" element={<ProductManagement />} />
       </Routes>
       {!shouldHideLayout && <Footer />}
     </>
